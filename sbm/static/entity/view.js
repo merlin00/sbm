e7754fa54ff27a2db5bb473e489bbda279bed488
권기덕 <merlin00@iMac.local>
2017-09-29 02:48:22 +0900

    editing

--- sbm/static/entity/view.js
+++ sbm/static/entity/view.js
@@ -1,14 +1,53 @@
 var entity_view_doc = document.currentScript.ownerDocument;
 var link = entity_view_doc.querySelector('link[rel="import"]');
-var template = link.import.querySelector('template');
+var tpl = link.import.querySelector('template');
+var hdr_len = 0;
  
-function attach_entity_view_to(selector, width='23em', height='30em') {
+function attach_entity_view_to(selector) {
     var view = entity_view_doc.querySelector('#view');
+    var list = entity_view_doc.querySelector('#_entities'); 
     
-    $(view).css('width', width)
-    $(view).css('height', height)
-    $(selector).append(view)
+    $(view).css('width', "100%");
+    $(view).css('height', "100%");
 
-    //$(view).find('#list').hide()
+    $(list).css('overflow-y', 'auto');
+    $(selector).append(view);
+
+    var a = $(view).children();
+
+    hdr_len += $(a[0]).height();
+    hdr_len += $(a[1]).height() * 2;
+    hdr_len += $(a[2]).height();
+    $(a[3]).height($(view).height() - hdr_len);
+
+    $(window).resize(function(){
+	$(a[3]).height($(view).height() - hdr_len)
+	console.log(hdr_len);
+    });
+
+    //$(view).find('#_entities').hide()
     $('.ui.dropdown').dropdown();
 }
+
+function add_item(topic) {
+    var item = entity_view_doc.importNode(tpl.content, true);
+    $(entity_view_doc).find('#_entities').append(item);
+}
+
+function test(){
+    console.log($(entity_view_doc).find('#_img').width());
+}
+
+$(function(){
+    $('div[onload]').trigger('onload');
+});
+
+
+add_item("test")
+add_item("test")
+add_item("test")
+add_item("test")
+add_item("test")
+add_item("test")
+add_item("test")
+add_item("test")
