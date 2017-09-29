284a9825642b107b7de852cd3ce3397654a417a9
Kwon Kideok <merlin00@pusan.ac.kr>
2017-09-29 17:44:28 +0900

    editing

--- sbm/static/entity/view.js
+++ sbm/static/entity/view.js
@@ -1,53 +1,51 @@
-var entity_view_doc = document.currentScript.ownerDocument;
-var link = entity_view_doc.querySelector('link[rel="import"]');
-var tpl = link.import.querySelector('template');
-var hdr_len = 0;
+var _entity_view_html;
+var _tpl;
+var _title_len = 0;
+
+function import_entity_view(href) {
+    var selector = 'link[rel="import"][href="'+ href +'"]'; 
+    _entity_view_html = document.querySelector(selector).import;
+    _tpl = _entity_view_html.querySelector('link[rel="import"]').import;
+    _tpl = _tpl.querySelector('template');
+}
  
 function attach_entity_view_to(selector) {
-    var view = entity_view_doc.querySelector('#view');
-    var list = entity_view_doc.querySelector('#_entities'); 
+    var view = $(_entity_view_html).find('#_entity');
+    var list = $(_entity_view_html).find('#_entity #_list'); 
     
     $(view).css('width', "100%");
     $(view).css('height', "100%");
 
     $(list).css('overflow-y', 'auto');
+
     $(selector).append(view);
 
-    var a = $(view).children();
+    var child = $(view).children();
 
-    hdr_len += $(a[0]).height();
-    hdr_len += $(a[1]).height() * 2;
-    hdr_len += $(a[2]).height();
-    $(a[3]).height($(view).height() - hdr_len);
+    _title_len += $(child[0]).height();
+    _title_len += $(child[1]).height() * 2;
+    _title_len += $(child[2]).height();
 
-    $(window).resize(function(){
-	$(a[3]).height($(view).height() - hdr_len)
-	console.log(hdr_len);
-    });
+    resize_entity_view();
+
+    $(window).resize(resize_entity_view);
 
     //$(view).find('#_entities').hide()
     $('.ui.dropdown').dropdown();
 }
 
+function resize_entity_view() {
+    $('#_entity #_list').height($('#_entity').height() - _title_len);
+}
+
 function add_item(topic) {
-    var item = entity_view_doc.importNode(tpl.content, true);
-    $(entity_view_doc).find('#_entities').append(item);
+    var item = _entity_view_html.importNode(_tpl.content, true);
+    $(document).find('#_entity #_list').append(item);
 }
 
-function test(){
-    console.log($(entity_view_doc).find('#_img').width());
-}
+$(document).ready(function() {
+    console.log('ready');
+    add_item("test");
 
-$(function(){
-    $('div[onload]').trigger('onload');
-});
+})
 
-
-add_item("test")
-add_item("test")
-add_item("test")
-add_item("test")
-add_item("test")
-add_item("test")
-add_item("test")
-add_item("test")
